75b4a8b46e7fe4104e4f565015b1b8114b5dadc7
yashkava <109713868+yashkava@users.noreply.github.com>
2022-08-26 22:59:13 +0530

    Add files via upload

---
+++ Module1/Practice/A1.java
@@ -0,0 +1,9 @@
+package a1;
+
+public class A1
+{
+	public void msg()
+	{
+		System.out.println("HELLO");
+	}
+}
---
+++ Module1/Practice/B1.java
@@ -0,0 +1,15 @@
+package b1;
+
+import a1.A1;
+
+public class B1 
+{
+	public static void main(String[] args) 
+	{
+		A1 a=new A1();
+		a.msg();
+		
+		
+		
+	}
+}
