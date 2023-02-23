065dd033cf11aee68b44c5a08576b7a79c9a6728
yashkava <109713868+yashkava@users.noreply.github.com>
2023-02-23 11:14:08 +0530

    Add files via upload

---
+++ Assignment M7/6.Text To Speak/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.QUEUE_ADD
+import android.widget.Button
+import android.widget.EditText
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    lateinit var edt1: EditText
+    lateinit var btn1: Button
+    lateinit var tts:TextToSpeech
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edt1 = findViewById(R.id.edt1)
+        btn1 = findViewById(R.id.btn1)
+        tts= TextToSpeech(applicationContext,this)
+
+        btn1.setOnClickListener {
+            var data=edt1.text.toString()
+            tts.speak(data,QUEUE_ADD,null)
+        }
+    }
+
+    override fun onInit(status: Int)
+    {
+        tts.setLanguage(Locale.US)
+        tts.setPitch(0.8F)
+        tts.setSpeechRate(0.5F)
+    }
+}
